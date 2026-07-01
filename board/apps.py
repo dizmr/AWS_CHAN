03ab467b19105c12ccf61026f67e3ef854b80113
drengrdiz <160162526+dizmr@users.noreply.github.com>
2026-07-01 20:33:49 +0300

    apps.py

--- board/apps.py
+++ board/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class BoardConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'board'
-    verbose_name = 'Дошка зізнань'
+    verbose_name = 'board'
